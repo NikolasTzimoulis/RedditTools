db45cdcd4048713da6d29ef3f045dd7bebd65727
Nikolas <naurgul@gmail.com>
2015-03-09 21:26:43 +0100

    Added option to make charts for single comments or for bundles of comments.

--- makeChart.py
+++ makeChart.py
@@ -3,16 +3,19 @@ import matplotlib.pyplot
 import os
 
 firstComment = 0
-lastComment = 10
+lastComment = 5
+allTogether = True
 
 con = sqlite3.connect("karmaHistory.db")
 with con:   
     cur = con.cursor()
     cur.execute("SELECT DISTINcT link FROM Comments")
     comments = [x[0] for x in cur.fetchall()]
-    for x in reversed(range(len(comments)-lastComment, len(comments)-firstComment)):
-        os.startfile(comments[x])
-        cur.execute("SELECT time, score FROM Comments WHERE link = ?", [comments[x]])
+    for c in reversed(range(len(comments)-lastComment, len(comments)-firstComment)):        
+        cur.execute("SELECT time, score FROM Comments WHERE link = ?", [comments[c]])
         scores = cur.fetchall()
         matplotlib.pyplot.plot([x[0] for x in scores], [y[1] for y in scores], 'ko:')
-        matplotlib.pyplot.show()
+        if not allTogether:
+            os.startfile(comments[c])
+            matplotlib.pyplot.show()
+    matplotlib.pyplot.show()
