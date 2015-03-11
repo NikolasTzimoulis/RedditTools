f7ec7f709f6cefb6dfcbd13934a09abf9cdee69b
Nikolas <naurgul@gmail.com>
2015-03-11 05:39:56 +0100

    Made makeChart more accurate but also slower
    - makeChart now finds all new comments in a specified time period (24 hours by default) instead of finding the last 10 comments added to the databse.

--- makeChart.py
+++ makeChart.py
@@ -1,21 +1,22 @@
 import sqlite3
 import matplotlib.pyplot
 import os
+import time
 
-firstComment = 0
-lastComment = 5
+timePeriod = 24*60*60
 allTogether = True
 
 con = sqlite3.connect("karmaHistory.db")
 with con:   
     cur = con.cursor()
-    cur.execute("SELECT DISTINcT link FROM Comments")
+    cur.execute("SELECT DISTINCT link FROM Comments")
     comments = [x[0] for x in cur.fetchall()]
-    for c in reversed(range(len(comments)-lastComment, len(comments)-firstComment)):        
-        cur.execute("SELECT time, score FROM Comments WHERE link = ?", [comments[c]])
+    for c in comments:        
+        cur.execute("SELECT time, score FROM Comments WHERE link = ?", [c])
         scores = cur.fetchall()
-        matplotlib.pyplot.plot([x[0] for x in scores], [y[1] for y in scores], 'ko:')
-        if not allTogether:
-            os.startfile(comments[c])
-            matplotlib.pyplot.show()
+        if min([x[0] for x in scores]) >= time.time()-timePeriod:
+            matplotlib.pyplot.plot([x[0] for x in scores], [y[1] for y in scores], 'ko:')
+            if not allTogether:
+                os.startfile(c)
+                matplotlib.pyplot.show()
     matplotlib.pyplot.show()
