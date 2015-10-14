ee818af78f57153355c8fc4c668b6ce2f49206fa
Nikolas <naurgul@gmail.com>
2015-10-15 00:43:39 +0200

    More flexibility in date formats
    - A variety of different date formats is now allowed.
    - Relative dates in the form "X years" are now allowed.
    - Date in submissions no longer needs to be in the beginning of the title or preceded by the word "to".
    - Fixed bug when last check date file is missing.
    - Removed obsolete variable.

--- dearfuture/collectIncoming.py
+++ dearfuture/collectIncoming.py
@@ -10,7 +10,8 @@ userName = 'futureMailBot'
 subreddit = 'DearFuture'
 calendarID = 'o8oq4ir40k0j1g7umacsuhq8tk@group.calendar.google.com'
 minScoreAllowed = 1
-delay = 2
+dateFormats = ["%Y/%m/%d", "%d/%m/%Y", "%m/%d/%Y", "%Y-%m-%d", "%d-%m-%Y", "%m-%d-%Y", "%Y.%m.%d", "%d.%m.%Y", "%m.%d.%Y"]
+now = datetime.datetime.utcnow()
 
 print "Collecting future mail..."
 
@@ -20,6 +21,7 @@ try:
     checkdateFile.close()
 except:
     lastId = None
+    lastCheckDate = 0
     
 try:
     passwordFile = open(passwordFileName, 'rb')
@@ -31,6 +33,20 @@ except:
     passwordFile = open(passwordFileName, 'wb')
     pickle.dump(passWord, passwordFile)
     passwordFile.close()
+    
+def getDates(words):
+    dates =[]
+    for i in range(len(words)):
+        for f in dateFormats:            
+            try:
+                d = datetime.datetime.strptime(words[i].strip(string.punctuation), f).date()
+                dates.append((d, i))
+                break
+            except:
+                continue
+        if words[i].strip(string.punctuation).isdigit() and len(words) > i+1 and words[i+1].startswith("year"):
+            dates.append((datetime.datetime(now.year+int(words[i]), now.month, now.day),i+1))
+    return dates
 
 r = praw.Reddit(user_agent=subreddit)
 r.login(userName, passWord, disable_warning=True)
@@ -40,32 +56,29 @@ updatedLastCheckDate = None
 for s in submissions:
     if s.created_utc <= lastCheckDate: break
     if s.score < minScoreAllowed: continue
-    titleWords = s.title.split()
-    if titleWords[0].strip(string.punctuation).lower() != "to": continue
-    try:
-        toDate = datetime.datetime.strptime(titleWords[1].strip(string.punctuation), "%Y/%m/%d").date()
-    except:
-        continue
-    fromDate = datetime.datetime.fromtimestamp(s.created_utc).date()
-    title = fromDate.strftime("FROM %Y/%m/%d: ") + ' '.join(titleWords[2:])
-    print s.title
-    googleCalendar.addEvent(title, toDate, description=s.permalink, source=s.url)
-    r.set_flair(subreddit, s, flair_text=u'sent', flair_css_class=u'sent')
-    if updatedLastCheckDate is None: updatedLastCheckDate = s.created_utc
+    words = s.title.split()
+    dates = getDates(words)
+    for d in dates:
+        toDate = d[0]
+        fromDate = datetime.datetime.fromtimestamp(s.created_utc).date()
+        title = fromDate.strftime("FROM %Y/%m/%d: ") + ' '.join(words[d[1]+1:])
+        if toDate > now.date():
+            print s.title
+            googleCalendar.addEvent(title, toDate, description=s.permalink, source=s.url)
+            r.set_flair(subreddit, s, flair_text=u'sent', flair_css_class=u'sent')
+            if updatedLastCheckDate is None: updatedLastCheckDate = s.created_utc
 
 
 for m in r.get_unread(limit=None):
     words = m.body.split()
-    for w in words:
-        try:
-            toDate = datetime.datetime.strptime(w.strip(string.punctuation), "%Y/%m/%d").date()
-            fromDate = datetime.datetime.fromtimestamp(m.created_utc).date()
-            title = fromDate.strftime("FROM %Y/%m/%d: ") + m.submission.title
-            if r.get_submission(m.permalink).comments[0].score < minScoreAllowed: continue
-            print m
-            googleCalendar.addEvent(title, toDate, description=m.body, source=m.permalink)
-        except:
-            continue 
+    dates = getDates(words)
+    for d in dates:
+        toDate = d[0]
+        fromDate = datetime.datetime.fromtimestamp(m.created_utc).date()
+        title = fromDate.strftime("FROM %Y/%m/%d: ") + m.submission.title
+        if r.get_submission(m.permalink).comments[0].score < minScoreAllowed: continue
+        print m
+        googleCalendar.addEvent(title, toDate, description=m.body, source=m.permalink)
     m.mark_as_read()
 
 if updatedLastCheckDate is not None:  
